d08e848b448f31b8d53e3de1b0dc49d20c3d7e1f
lee gyu yeol-mac <i1004gy@gmail.com>
2025-07-01 16:39:03 +0900

    feat: 타입체크

---
+++ app/schemas/schema_data.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+from typing import List, Dict, Any
+
+SchemaMap = Dict[str, Any]  # 실제 schema json 구조 그대로 받을 경우
+
+class SchemaData(BaseModel):
+    details: List[SchemaMap]  # 각 schema의 상세 정보 리스트
+
+class SchemaResponseModel(BaseModel):
+    message: str
+    data: SchemaData
