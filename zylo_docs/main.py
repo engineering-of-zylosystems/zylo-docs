7332eee29d0b30f9f921ee430c100d107be71ee8
lee gyu yeol-mac <i1004gy@gmail.com>
2025-07-03 16:57:42 +0900

    폴더 구조 변경

---
+++ zylo_docs/main.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from zylo_docs.schemas.response import APIErrorDetail, APIErrorResponse, APIResponse
+from zylo_docs.routers import schemas
+from zylo_docs.exceptions import APIException
+from zylo_docs.schema_generator import save_openapi_json
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await save_openapi_json(app)
+    yield
+app = FastAPI(title="Zylo Docs API", lifespan=lifespan)
+
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+@app.get("/health")
+def health_check():
+    return APIResponse(
+        success=True,
+        message="Zylo-hub-be API server is running.",
+        data=None
+    )
+
+app.include_router(schemas.router, prefix="/schemas", tags=["schemas"])
+
+# 모든 경로에 대해 정적 프론트 서빙
+@app.get("/")
+async def serve_react_app():
+    print(f"Serving React app")
+    return FileResponse("static/index.html")
+
+@app.exception_handler(APIException)
+async def api_exception_handler(request: Request, exc: APIException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content=APIErrorResponse(
+            success=False,
+            message=exc.message,
+            data=APIErrorDetail(
+                code=exc.error.code,
+                details=exc.error.details
+            )
+        ).model_dump()
+    )
