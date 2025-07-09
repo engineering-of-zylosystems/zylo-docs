7a28c907a6a51a8562981a767294f7840757e2c1
lee gyu yeol-mac <i1004gy@gmail.com>
2025-07-09 09:46:14 +0900

    [#3] feat:  필요없는 코드 삭제

--- zylo_docs/services/user_server_service.py
+++ zylo_docs/services/user_server_service.py
@@ -1,25 +1,13 @@
-from pathlib import Path
-import json
+from collections import defaultdict
+import copy
 
-async def get_user_schemas():
-    BASE_DIR = Path(__file__).resolve().parent.parent
-    file_path = BASE_DIR / "data" / "all_schemas.json"
-    print(file_path)
+async def get_user_schemas(request):
     schemas = []
 
-    try:
-        with open(file_path, "r", encoding="utf-8") as f:
-            openapi_json = json.load(f)
-    except FileNotFoundError:
-        print(f"File not found: {file_path}")
+    openapi_json = request.app.openapi()
+    if openapi_json.get("components") is None:
+        print("OpenAPI data does not contain 'components' key.")
         return None
-    except json.JSONDecodeError:
-        print(f"Failed to decode JSON from file: {file_path}")
-        return None
-    except Exception as e:
-        print(f"Unexpected error while reading the schema file: {e}")
-        return None
-
     components = openapi_json.get("components")
     if not components:
         print("'components' key not found in the OpenAPI data.")
@@ -37,6 +25,7 @@ async def get_user_schemas():
         return None
     
     return schemas
+
 def parse_openapi_paths(paths):
     grouped = defaultdict(list)
 
@@ -64,6 +53,7 @@ async def get_user_operation(request):
     openapi_json = request.app.openapi()
     result = parse_openapi_paths(openapi_json.get("paths", {}))
     return result
+
 def resolve_ref(obj, components):
     if isinstance(obj, dict):
         if "$ref" in obj:
@@ -80,6 +70,7 @@ def resolve_ref(obj, components):
         return [resolve_ref(item, components) for item in obj]
     else:
         return obj
+
 def parse_openapi_paths_by_id(paths, components, url, target_method):
     path_item = paths.get(url, {})
     if not path_item:
@@ -96,6 +87,7 @@ def parse_openapi_paths_by_id(paths, components, url, target_method):
             info["responses"] = resolve_ref(info["responses"], components)
 
     return resolved_path_item
+
 async def get_user_operation_by_id(request, url, method):
     
     openapi_json = request.app.openapi()
