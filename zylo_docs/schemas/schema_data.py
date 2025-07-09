8d150510400abfc4e369566c42f086653fb492f9
lee gyu yeol-mac <i1004gy@gmail.com>
2025-07-09 10:27:49 +0900

    [#3] feat: path를 operation으로 변경

--- zylo_docs/schemas/schema_data.py
+++ zylo_docs/schemas/schema_data.py
@@ -8,7 +8,7 @@ class SchemaResponseModel(BaseModel):
 
 class APIRequestModel(BaseModel):
     method: str
-    path: str
+    operation: str
     path_params: Optional[Dict[str, Any]] = None
     query_params: Optional[Dict[str, Any]] = None
     body: Optional[Dict[str, Any]] = None
