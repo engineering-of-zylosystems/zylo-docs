c90d074ebff2bf3b2c44a48b4f8247f94069f493
lee gyu yeol-mac <i1004gy@gmail.com>
2025-07-09 09:40:29 +0900

    [#3]feat: 테스트 api

--- zylo_docs/routers/front_api.py
+++ zylo_docs/routers/front_api.py
@@ -1,13 +1,16 @@
 from fastapi import APIRouter,Query, Request,HTTPException
 from zylo_docs.services.user_server_service import get_user_schemas, get_user_operation,get_user_operation_by_id
 from zylo_docs.schemas.schema_data import SchemaResponseModel
+from zylo_docs.schemas.schema_data import APIRequestModel
+from fastapi.responses import JSONResponse
+import httpx
 
 router = APIRouter()
 @router.get("/schemas",response_model=SchemaResponseModel, include_in_schema=False)
 async def get_schemas(request: Request):
     try:
         result = await get_user_schemas(request)
-        if result is None or not result: # None이거나 빈 리스트일 때 404 처리 (스키마가 없는 것도 자원 없음으로 간주)
+        if result is None or not result: 
             raise HTTPException(
                 status_code=404,
                 detail={
@@ -60,7 +63,6 @@ async def get_operation_by_id(
     method: str = Query(..., description="HTTP 메소드 (예: GET, POST 등)")
 ):
     result = await get_user_operation_by_id(request, url, method)
-    print(result)
     if not result:
         raise HTTPException(
             status_code=404,
@@ -78,3 +80,36 @@ async def get_operation_by_id(
         "message": "Operation retrieved successfully",
         "data": result
     }
+
+@router.post("/test-executions", include_in_schema=False)
+async def test_executions(request: Request, request_data: APIRequestModel):
+
+    target_path = request_data.path
+    if request_data.path_params:
+        for key, value in request_data.path_params.items():
+            placeholder = f":{key}"
+            target_path = target_path.replace(placeholder, str(value))
+    transport = httpx.ASGITransport(app=request.app)
+
+    async with httpx.AsyncClient(transport=transport, base_url="http://") as client:
+        response = await client.request(
+            method=request_data.method,
+            url=target_path,
+            params=request_data.query_params,
+            json=request_data.body
+        )
+        if 200 <= response.status_code < 300:
+            return {
+                "success": True,
+                "message": "Request executed successfully",
+                "data": response.json() if response.content else None
+            }
+        return JSONResponse(
+            status_code=400,
+            content={
+                "success": False,
+                "message": "Test failed",
+                "code": "INTERNAL_LOGIC_TEST_FAILED",
+                "data": response.json() if response.content else None
+            }
+        )
