0f6d1a0f7518548bb970f9f480aa048dba4b38e4
조경호(Kyungho Jo) <32837316+jokh0108@users.noreply.github.com>
2025-07-14 15:30:40 +0900

    feat: open api json service

--- zylo_docs/integration.py
+++ zylo_docs/integration.py
@@ -16,6 +16,8 @@ def set_initial_openapi_spec(app: FastAPI):
     openapi_service.set_latest(openapi_json)
 
 def add_zylo_docs(app: FastAPI):
+    set_initial_openapi_spec(app)
+
     app.include_router(front_route.router, prefix="/zylo-docs", tags=["schemas"])
     app.include_router(proxy_route.router, prefix="/zylo-docs/api", tags=["proxy"])
     app.mount("/static", StaticFiles(directory=os.path.join(os.path.dirname(__file__), "static")), name="static")
