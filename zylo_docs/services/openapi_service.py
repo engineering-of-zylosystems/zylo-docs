54d067bb36f082a6c1f7b6cd303a1030990a6464
lee gyu yeol-mac <i1004gy@gmail.com>
2025-07-15 16:30:01 +0900

    [#5]feat: 인스턴스 클래스 정의

--- zylo_docs/services/openapi_service.py
+++ zylo_docs/services/openapi_service.py
@@ -1,16 +1,14 @@
-from typing import List, Optional
+from typing import Optional
 
 class OpenApiService:
     def __init__(self):
-        self.openapi_json_list: List[dict] = []
-
-    def set_latest(self, json: dict):
-        self.openapi_json_list.append(json)
-
-    def get_latest(self) -> dict:
-        if self.openapi_json_list:
-            return self.openapi_json_list[-1]
-        return {}
-
-# 싱글턴 인스턴스 생성
-openapi_service = OpenApiService()
+        self.current_spec: Optional[dict] = None
+        
+    def set_current_spec(self, openapi_json: dict):
+        self.current_spec = openapi_json
+        
+    def get_current_spec(self) -> dict:
+        if self.current_spec is None:
+            return {}
+        return self.current_spec
+        
