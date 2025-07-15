411c63a22478643750058dc80d98c4b138c88fcd
lee gyu yeol-mac <i1004gy@gmail.com>
2025-07-15 16:25:36 +0900

    [#5] feat: 인스턴스 초기화

--- zylo_docs/integration.py
+++ zylo_docs/integration.py
@@ -1,4 +1,3 @@
-# zylo_docs/server_components.py
 import os
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
@@ -6,18 +5,18 @@ from fastapi.responses import FileResponse
 from zylo_docs.schemas.response import APIResponse
 from .routers import front_route, proxy_route 
 from .middlewares.exception_handler import ExceptionHandlingMiddleware
-from zylo_docs.services.openapi_service import openapi_service 
+from zylo_docs.services.openapi_service import OpenApiService 
 
 EXTERNAL_API_BASE = "https://api.zylosystems.com"
 PROXY_PREFIX = "/zylo-docs/api"
-
 def set_initial_openapi_spec(app: FastAPI):
     openapi_json = app.openapi()
-    openapi_service.set_latest(openapi_json)
+    app.state.openapi_service.set_current_spec(openapi_json)
 
 def add_zylo_docs(app: FastAPI):
-    set_initial_openapi_spec(app)
-
+    if not hasattr(app.state, 'openapi_service'):
+        app.state.openapi_service = OpenApiService()
+    
     app.include_router(front_route.router, prefix="/zylo-docs", tags=["schemas"])
     app.include_router(proxy_route.router, prefix="/zylo-docs/api", tags=["proxy"])
     app.mount("/static", StaticFiles(directory=os.path.join(os.path.dirname(__file__), "static")), name="static")
