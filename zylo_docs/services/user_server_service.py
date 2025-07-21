d331fc0d8e8e25b06de1b4202cc2c9e1ac582ea8
lee gyu yeol-mac <i1004gy@gmail.com>
2025-07-21 16:05:47 +0900

    feat/ testcase 파싱

--- zylo_docs/services/user_server_service.py
+++ zylo_docs/services/user_server_service.py
@@ -47,6 +47,33 @@ def resolve_ref(obj, components):
         return [resolve_ref(item, components) for item in obj]
     else:
         return obj
+def resolve_testcase_inputs(info, components):
+    inputs = []
+    testcase = {}
+    parameters = info.get("parameters", [])
+    for param in parameters:
+        if param.get("in")=="query":
+            for key, value in param["examples"].items():
+                if key not in testcase:
+                    testcase[key] = {}
+                if testcase[key].get("query_params") is None:
+                    testcase[key]["query_params"] = {}
+                testcase[key]["query_params"][param["name"]] = value.get("value")
+        if param.get("in")=="path":
+            for key, value in param["examples"].items():
+                if key not in testcase:
+                    testcase[key] = {}
+                if testcase[key].get("path_params") is None:
+                    testcase[key]["path_params"] = {}
+                testcase[key]["path_params"][param["name"]] = value.get("value")
+    requestBody_examples = info.get("requestBody", {}).get("content",{}).get("application/json", {}).get("examples", {})
+    for key, value in requestBody_examples.items():
+        if key not in testcase:
+            testcase[key] = {}
+        if testcase[key].get("body") is None:
+            testcase[key]["body"] = {}
+        testcase[key]["body"] = value
+    return testcase
 
 def parse_openapi_paths_by_method(paths, components, path, target_method):
     path_item = paths.get(path, {})
@@ -54,21 +81,22 @@ def parse_openapi_paths_by_method(paths, components, path, target_method):
         return None
 
     resolved_path_item = copy.deepcopy(path_item)
-
     for method, info in resolved_path_item.items():
-        
         if method != target_method:
             continue
         if info.get("requestBody"):
             info["requestBody"] = resolve_ref(info["requestBody"], components)
         if info.get("responses"):
             info["responses"] = resolve_ref(info["responses"], components)
+        testcase = resolve_testcase_inputs(info, components)
+        info["testcase_inputs"] = testcase
+        
     return resolved_path_item
 
-async def get_user_operation_by_id(request, path, method):
-
+async def get_user_operation_by_path(request, path, method):
     service: OpenApiService = request.app.state.openapi_service
     openapi_json = service.get_current_spec()
     components = openapi_json.get("components", {})
+
     result = parse_openapi_paths_by_method(openapi_json.get("paths", {}), components, path, method)
     return result
