2c8d8f2739c819d43e61a429a4ed4e6e773402ca
lee gyu yeol-mac <i1004gy@gmail.com>
2025-07-24 15:28:36 +0900

    /feat 로깅 삭제하는 코드

--- zylo_docs/integration.py
+++ zylo_docs/integration.py
@@ -6,9 +6,10 @@ from zylo_docs.schemas.response import APIResponse
 from .routers import front_route, proxy_route 
 from .middlewares.exception_handler import ExceptionHandlingMiddleware
 from zylo_docs.services.openapi_service import OpenApiService 
+from zylo_docs.logging import NoZyloDocsLogFilter
+# 로깅 삭제하는 코드 개발 모드에서는 주석 처리
+NoZyloDocsLogFilter().setup_logging()
 
-EXTERNAL_API_BASE = "https://api.zylosystems.com"
-PROXY_PREFIX = "/zylo-docs/api"
 def set_initial_openapi_spec(app: FastAPI):
     openapi_json = app.openapi()
     app.state.openapi_service.set_current_spec(openapi_json)
@@ -16,6 +17,9 @@ def set_initial_openapi_spec(app: FastAPI):
 def add_zylo_docs(app: FastAPI):
     if not hasattr(app.state, 'openapi_service'):
         app.state.openapi_service = OpenApiService()
+        
+    app.mount("/static", StaticFiles(directory=os.path.join(os.path.dirname(__file__), "static")), name="static")
+    app.include_router(front_route.router, prefix="/zylo-docs", tags=["front"])
     
     app.include_router(front_route.router, prefix="/zylo-docs", tags=["schemas"])
     app.include_router(proxy_route.router, prefix="/zylo-docs/api", tags=["proxy"])
