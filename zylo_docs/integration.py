f047ce8482946fb8a2565334484279db270cbcbd
lee gyu yeol-mac <i1004gy@gmail.com>
2025-07-25 10:55:02 +0900

    /feat fron-update

--- zylo_docs/integration.py
+++ zylo_docs/integration.py
@@ -2,13 +2,12 @@ import os
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-from zylo_docs.schemas.response import APIResponse
 from .routers import front_route, proxy_route 
 from .middlewares.exception_handler import ExceptionHandlingMiddleware
 from zylo_docs.services.openapi_service import OpenApiService 
 from zylo_docs.logging import NoZyloDocsLogFilter
 # 로깅 삭제하는 코드 개발 모드에서는 주석 처리
-NoZyloDocsLogFilter().setup_logging()
+# NoZyloDocsLogFilter().setup_logging()
 
 
 def set_initial_openapi_spec(app: FastAPI):
@@ -31,4 +30,3 @@ def add_zylo_docs(app: FastAPI):
         return FileResponse(os.path.join(os.path.dirname(__file__), "static", "index.html"))
 
     set_initial_openapi_spec(app)
-#
