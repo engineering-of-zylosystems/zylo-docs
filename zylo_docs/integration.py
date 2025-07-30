6fade2a18527591501cc1594509d64a71861abec
lee gyu yeol-mac <i1004gy@gmail.com>
2025-07-30 19:55:42 +0900

    fix

--- zylo_docs/integration.py
+++ zylo_docs/integration.py
@@ -7,7 +7,7 @@ from .middlewares.exception_handler import ExceptionHandlingMiddleware
 from zylo_docs.services.openapi_service import OpenApiService 
 from zylo_docs.logging import NoZyloDocsLogFilter
 # 로깅 삭제하는 코드 개발 모드에서는 주석 처리
-# NoZyloDocsLogFilter().setup_logging()
+NoZyloDocsLogFilter().setup_logging()
 HOST = os.getenv("SERVER_HOST", "localhost")
 PORT = os.getenv("SERVER_PORT", "8000")
 
@@ -31,4 +31,4 @@ def add_zylo_docs(app: FastAPI):
         return FileResponse(os.path.join(os.path.dirname(__file__), "static", "index.html"))
 
     set_initial_openapi_spec(app)
-#
+
