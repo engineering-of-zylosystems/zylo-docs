f6f96061ba4bd6c2e9acfc26101fa561aae6b04b
lee gyu yeol-mac <i1004gy@gmail.com>
2025-08-13 14:10:34 +0900

    [#102] env var setting

--- zylo_docs/config.py
+++ zylo_docs/config.py
@@ -1,2 +1,8 @@
-EXTERNAL_API_BASE = "https://api.zylosystems.com/v1"
-# EXTERNAL_API_BASE = "http://0.0.0.0:8001/v1"
+import os
+from dotenv import load_dotenv
+
+# config.py 파일의 위치를 기준으로 상위 폴더의 .env 파일을 찾습니다.
+dotenv_path = os.path.join(os.path.dirname(__file__), "..", ".env")
+load_dotenv(dotenv_path=dotenv_path)
+
+EXTERNAL_API_BASE = os.getenv("EXTERNAL_API_BASE", "https://api.zylosystems.com/v1")
